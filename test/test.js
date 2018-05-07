c86f48cbe02cd089117f7d11a7705d6f588ae17d
René <rene.schwarzinger@milleniumfrog.de>
2018-05-07 08:57:31 +0200

    node test update

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ let logupTsTransportFile = require('../dist/umd/logupts-transport-file').LogUpTs
 let path  = require('path');
 
 describe('run logs', () => {
-    it ('', () => {
+    it ('log hello and bye to console and save it in {{date}}.log', () => {
         let logger = new logupts({quiet: true});
         logger.loguptsOptions.transports[0] = new logupTsTransportFile(
             path.resolve(__dirname, 'log'), '{{date}}.log', logger, ['ALL']
@@ -13,24 +13,28 @@ describe('run logs', () => {
                 return logger.log('bye');
             })
     });
-    it ('', () => {
+    it ('log the Number 0-99 and save in file, log an error after that', () => {
         let logger = new logupts({quiet: true});
+        let PromArr = [];
         logger.loguptsOptions.transports[0] = new logupTsTransportFile(
             path.resolve(__dirname, 'log'), '{{date}}.log', logger, ['ALL']
         )
         for (let i = 0; i < 100; ++i) {
-            logger.log(String(i));
+            PromArr.push(logger.log(String(i)));
         }
-        logger.error('runned');
+        PromArr.push(logger.error('runned'));
+        return Promise.all(PromArr);
     });
-    it ('', () => {
+    it ('only log the error', () => {
         let logger = new logupts({quiet: true});
+        let PromArr = [];
         logger.loguptsOptions.transports[0] = new logupTsTransportFile(
-            path.resolve(__dirname, 'log'), '{{date}}.err.log', logger, ['ERROR']
+            path.resolve(__dirname, 'log'), '{{date}}.error.log', logger, ['ERROR']
         )
         for (let i = 0; i < 100; ++i) {
-            logger.log(String(i));
+            PromArr.push(logger.log(String(i)));
         }
-        logger.error('runned');
+        PromArr.push(logger.error('runned'));
+        return Promise.all(PromArr);
     });
 })
