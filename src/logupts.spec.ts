256e3899ca1dc0062b9c00b736cd56900e0779bb
René Schwarzinger <rene@Renes-MacBook-Pro.local>
2018-07-28 22:55:36 +0200

    added tests

--- src/logupts.spec.ts
+++ src/logupts.spec.ts
@@ -1,5 +1,5 @@
-import { LogUpTs, LogUpTsOptions } from './logupts';
-describe( 'test', () => {
+import { LogUpTs, LogUpTsOptions, Placeholder } from './logupts';
+describe( 'LogUpTs', () => {
     it( 'log to console without Transport and customFunctions', async () => {
         let logger: LogUpTs = new LogUpTs( <LogUpTsOptions>{ quiet: true } );
         let l: string = await logger.log( 'hello' );
@@ -10,9 +10,43 @@ describe( 'test', () => {
         expect( l ).toEqual( '2018 hello' );
     } );
 
-    it( 'log error to console without Transport and customFunctions', async () => {
+    it( 'log error to console without Transport and customFunctions, without Stack', async () => {
         let logger: LogUpTs = new LogUpTs( <LogUpTsOptions>{ quiet: true, logStack: false } );
         let l: string = await logger.error( new Error( 'a Error happend') );
         expect( l ).toEqual( '[ERROR] a Error happend' );
-    } );
+    });
+
+    it( 'log error to console without Transport and customFunctions, with Stack', async () => {
+        let logger: LogUpTs = new LogUpTs( <LogUpTsOptions>{ quiet: true, logStack: true } );
+        let l: string = await logger.error( new Error( 'a Error happend') );
+        if ( l.length < 100 ) {
+            throw new Error( 'string is a lot to short' );
+        }
+    });
+
+    it( 'add a new Placeholder and replace the defaultplaceholders', async () => {
+        let conf: LogUpTsOptions = {
+            quiet: true,
+            prefix: '',
+            placeholders: [
+                {
+                    keys: ['<a>', '</a>'],
+                    replacer: ( abs?: string, passArg?: any ) => {
+                        return abs || '';
+                    },
+                    flags: 'g',
+                },
+                {
+                    keys: ['<maintainer />'],
+                    flags: 'g',
+                    replacer: () => {
+                        return 'milleniumfrog';
+                    }
+                }
+            ],
+        };
+        let logger: LogUpTs = new LogUpTs( conf, {} );
+        let str: string = await logger.log( '<a>hello <maintainer /></a>' );
+        expect( str ).toEqual( 'hello milleniumfrog' );
+    })
 });
