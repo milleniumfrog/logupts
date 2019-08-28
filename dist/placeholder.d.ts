46a83f5610df21e6bfccd17deefcf4f345dc50e8
René Schwarzinger <web@milleniumfrog.de>
2019-08-28 10:45:13 +0200

    chore: prepare v4 with deleting alls old files

--- dist/placeholder.d.ts
+++
@@ -1,4 +0,0 @@
-import { ComplexKey as Placeholder } from 'strplace';
-export { ComplexKey as Placeholder, replaceComplex as replacePlaceholder } from 'strplace';
-export declare const DefaultPlaceholders: Placeholder[];
-export declare function fillStrWithZeros(length: number, msg: string): string;
--- src/placeholder.spec.ts
+++
@@ -1,9 +0,0 @@
-import { Placeholder, fillStrWithZeros } from './placeholder';
-import { expect } from 'chai';
-
-describe( 'Placeholders', () => {
-    it( 'fillStrWithZeros', () => {
-        let str = fillStrWithZeros(4, '1');
-        expect( str ).to.eql( '0001' );
-    });
-});
